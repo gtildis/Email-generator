10a7e406d095008ece55e88477851cd8ad498a9d
gtildis <gretil_15@hotmail.com>
2024-03-30 13:27:27 +0100

    add route on app.js

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ require("./config")(app);
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
+// Companies routes
+const companiesRouter = require("./routes/companies.routes");
+app.use("/api", companiesRouter);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
